606f2e373c241e737ad15556b3a7e1142e19374f
hafizurbagjan <hafizurbagjan@gmail.com>
2025-02-10 00:23:58 +0600

    250210004 commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-alert("You are barred from copying the content of the site!");
 document.addEventListener('contextmenu', event => event.preventDefault());
 document.addEventListener('selectstart', event => event.preventDefault());
 document.addEventListener('copy', event => event.preventDefault());
