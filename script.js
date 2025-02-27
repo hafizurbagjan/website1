e414d9a2e69880a84d9caddccffe395d4cf996dd
hafizurbagjan <hafizurbagjan@gmail.com>
2025-02-27 08:03:23 +0600

    BCB First Commit

--- script.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.addEventListener('selectstart', event => event.preventDefault());
-document.addEventListener('copy', event => event.preventDefault());
